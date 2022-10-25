fa88ef5a52677a358ae8416aa9df33f0ff398222
Kyron4251 <101371579+Kyron4251@users.noreply.github.com>
2022-10-25 10:16:28 -0300

    Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { NavigationBar } from "./components/NavigationBar";
 import { Carousel_first } from "./components/Carousel_first";
 import { PlaceHolder_1 } from "./components/PlaceHolder_1";
 import { PlaceHolder_2 } from "./components/PlaceHolder_2";
+import { Footer } from "./components/Footer";
+
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -24,6 +26,7 @@ export const App = () => {
               <PlaceHolder_1 />
               <PlaceHolder_2 />
               <PlaceHolder_2 />
+              <Footer/>
             </>
           }
         />
