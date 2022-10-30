5841599959884abc2e8c907d93af3a092d9124b2
Benjamin <benja.lillo.u@gmail.com>
2022-10-30 16:05:11 -0300

    agrego cosas al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,91 +7,126 @@ import {
   MDBIcon,
   MDBBtn
 } from 'mdb-react-ui-kit';
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
 
 
 export const Footer = () => {
   return (
-    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
-      <MDBContainer className='pt-4'>
-        <section className='mb-4'>
-          <MDBBtn
-            rippleColor="dark"
-            link
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fab fa-facebook-f' />
-          </MDBBtn>
+    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#484848' }}>
+      
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom' >
+        <div className='me-5 d-none d-lg-block'>
+          <span>Nuestras otras redes sociales:</span>
+        </div>
 
-          <MDBBtn
-            rippleColor="dark"
-            link
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-twitter' />
-          </MDBBtn>
+        <div>
+          <a href='' className='me-4 text-reset'>
+            <MDBIcon fab icon="facebook-f" />
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <MDBIcon fab icon="twitter" />
+          </a>
+          <a href='https://www.instagram.com/ryodan.comiqueria/' className='me-4 text-reset'>
+            <MDBIcon fab icon="instagram" />
+          </a>
+        </div>
+      </section>
 
-          <MDBBtn
-            rippleColor="dark"
-            link
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-google' />
-          </MDBBtn>
+      <section className=''>
+        <MDBContainer className='text-center text-md-start mt-5'>
+          <MDBRow className='mt-3'>
+            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                <MDBIcon fas icon="book-open" className="me-3" />
+                Ryodan-Comiquería
+              </h6>
+              <p>
+              Comiqueria Online Chilena 🇯🇵📚🇨🇱
+Compra seguro, recibe rápido 🚛              </p>
+            </MDBCol>
 
-          <MDBBtn
-            rippleColor="dark"
-            link
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-instagram' />
-          </MDBBtn>
+            {/* <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Productos</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  a
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  b
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  c
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  d
+                </a>
+              </p>
+            </MDBCol>
 
-          <MDBBtn
-            rippleColor="dark"
-            link
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-linkedin' />
-          </MDBBtn>
+            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Otros links</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Inicio
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Productos
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Nosotros
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Carrito
+                </a>
+              </p>
+              <Nav><Nav.Link to="/shopping_cart" as={NavLink}>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Carrito
+                </a>
+              </p>
+              </Nav.Link></Nav>
+              
+            </MDBCol> */}
 
-          <MDBBtn
-            rippleColor="dark"
-            link
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-github' />
-          </MDBBtn>
-        </section>
-      </MDBContainer>
+            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contacto</h6>
+              <p>
+                <MDBIcon icon="home" className="me-2" />
+                Chile, CL
+              </p>
+              <p>
+                <MDBIcon icon="envelope" className="me-3" />
+                info@example.com
+              </p>
+              <p>
+                <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
+              </p>
+              <p>
+                <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
 
       <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2020 Copyright:
-        <a className='text-dark' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+        <a className='text-dark' href='https://www.instagram.com/ryodan.comiqueria/'>
+          Ryodan-Comiquería
         </a>
       </div>
     </MDBFooter>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import "./custom.css";
 import { App } from "./App"; // Cambio
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
